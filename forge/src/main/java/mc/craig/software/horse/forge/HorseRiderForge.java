c457afcca4f0ca51d465a1318a0abc4ceb74a5e7
Craig <sandedshoes@gmail.com>
2024-08-26 19:50:47 +0100

    This mod somehow takes more time to update its workspace than the code itself

--- forge/src/main/java/mc/craig/software/horse/forge/HorseRiderForge.java
+++ forge/src/main/java/mc/craig/software/horse/forge/HorseRiderForge.java
@@ -1,7 +1,7 @@
 package mc.craig.software.horse.forge;
 
 import mc.craig.software.horse.HorseRider;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 
 @Mod(HorseRider.MOD_ID)
 public class HorseRiderForge {
